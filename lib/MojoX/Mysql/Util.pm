e170b7b530be7a0142836c27ffce2cb049cb5b1a
Kostya Ten <kostya@yandex-team.ru>
2016-02-26 18:38:59 +0000

    restore code

--- lib/MojoX/Mysql/Util.pm
+++ lib/MojoX/Mysql/Util.pm
@@ -15,6 +15,16 @@ sub quote {
 	}
 }
 
+sub quote_in {
+	my ($self,$obj,$default) = @_;
+	if(ref $obj eq 'ARRAY'){
+		return join(",",map($self->quote($_,$default),@{$obj}));
+	}
+	else{
+		return $self->quote($obj,$default);
+	}
+}
+
 sub id {
 	my ($self) = @_;
 	my @keys = sort {$a <=> $b} grep($_ ne '_default',keys %{$self->{'config'}});
